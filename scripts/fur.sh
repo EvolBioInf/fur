fe313a8af7dbe0f33d26ba4e5465ecd27e2d1c5e
Bernhard Haubold <haubold@evolbio.mpg.de>
2020-06-25 18:21:43 +0200

    User feedback during test

--- scripts/fur.sh
+++ scripts/fur.sh
@@ -1,7 +1,13 @@
+echo -n "# Download data..."
 wget -q guanine.evolbio.mpg.de/fur/test.tar.gz
 tar -xzf test.tar.gz
+echo "done."
+echo -n "# Make fur database..."
 ./makeFurDb -t testTar -n testNei -d furDb 2>/dev/null
+echo "done."
+echo -n "# Run fur..."
 ./fur -d furDb > tmp.out 2>/dev/null
+echo "done."
 DIFF=$(diff tmp.out ../data/fur.out)
 if [ "$DIFF" == "" ] 
 then
