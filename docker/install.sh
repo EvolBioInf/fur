bf711364707b5209833c3a2408708e5a5cab0dc8
Bernhard Haubold <haubold@evolbio.mpg.de>
2020-12-03 15:30:51 +0100

    Revised docker package
    This now contains the documentation and is build completely from
    sources. This means that the file install.sh contains a complete
    description of the installation. The compiler tool-chain is removed
    after the container is built to keep the image slim.

---
+++ docker/install.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Bash "strict mode"
+set -euo pipefail
+# Noninteractive
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get -y upgrade
+apt-get -y install autoconf build-essential curl git gnuplot libbsd-dev \
+    libbsd0 libdivsufsort-dev libdivsufsort3 libgsl-dev libgsl23 \
+    libsdsl-dev libsdsl3 ncbi-blast+ noweb primer3 sudo \
+    texlive-latex-extra texlive-latex-recommended texlive-pstricks \
+    texlive-science
+apt-get clean
+rm -rf /var/lib/apt/lists/*
+# Install macle
+git clone https://github.com/evolbioinf/macle
+cd macle
+make
+cp build/macle /usr/local/bin
+cd ..
+rm -rf macle
+# Install phylonium
+git clone https://github.com/evolbioinf/phylonium
+cd phylonium
+autoreconf -fi -Im4
+./configure
+make
+cp src/phylonium /usr/local/bin
+cd ..
+rm -rf phylonium
+# Install and test fur
+git clone https://github.com/evolbioinf/fur
+cd fur
+make
+cp build/* /usr/local/bin
+make doc
+cp doc/fur.pdf /usr/local/share/
+cd ..
+rm -rf fur
+# Clean up
+apt-get -y remove autoconf build-essential git gnuplot libbsd-dev \
+    libdivsufsort-dev libgsl-dev libsdsl-dev noweb \
+    texlive-latex-extra texlive-latex-recommended texlive-pstricks \
+    texlive-science
+apt clean
+apt -y autoremove
