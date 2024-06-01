0adb1818a0b976ea92297e2d1d51262620346787
Bernhard Haubold <haubold@evolbio.mpg.de>
2024-06-01 12:18:27 +0200

    First ensure sudo

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,10 @@
-sudo apt update
-sudo apt -y upgrade
-sudo apt -y autoremove
-sudo apt -y install git golang libdivsufsort-dev make ncbi-blast+ phylonium
+s=$(which sudo)
+if [[ $s == "" ]]; then
+    apt update
+    apt -y install sudo
+fi
+h=$(history | tail | grep update)
+if [[ $h == "" ]]; then
+    sudo apt update
+fi
+sudo apt -y install golang libdivsufsort-dev make ncbi-blast+ phylonium
